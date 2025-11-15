0e4d8210604e19547b395b31fe197bcc1dca2d50
sahana-gudi <sonugudi472@gmail.com>
2025-11-15 11:47:10 +0530

    Create employe.py

---
+++ employe.py
@@ -0,0 +1,9 @@
+name="ABC"
+id=100
+address="XYZ"
+salary=90000
+print("Employe detailes:")
+print("Employe name is:"name)
+print("Employe ID is:"id)
+print("Employe address is:"address)
+print("Employe salary is:"salary)
